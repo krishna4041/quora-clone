98c2f2665f4bf4bacd40824fe0b20bb4844fe9f6
krishna4041 <krishna.5053@gmail.com>
2018-08-14 01:09:47 +0530

    make it better

--- quora/settings.py
+++ quora/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '@x@3qw@t9o*gt1re^36*c48!j=p^p8_q41fn*!f+^ngn&+73g2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com"]
 
 
 # Application definition
